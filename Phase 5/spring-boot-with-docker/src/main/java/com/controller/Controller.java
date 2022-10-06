9013c74f10cb66ce0762ba5bfda38bbc1132d7db
Ramanarayani <ramanarayanimuthuraman@gmail.com>
2022-10-06 21:24:54 +0530

    done

---
+++ Phase 5/spring-boot-with-docker/src/main/java/com/controller/Controller.java
@@ -0,0 +1,17 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Controller {
+
+	@GetMapping(value="/")
+	public String greeting() {
+		return "Welcome to spring boot with docker by Ram";
+	}
+	@GetMapping(value= "say/{name}")
+	public String sayHello(@PathVariable("name") String name) {
+		return "Welcome user " +name+ " to spring boot with docker";
+	}
+}
